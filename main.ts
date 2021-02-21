699378968af791bbee8660456c610ade5cdd482f
samuelpugh4 <44142805+samuelpugh4@users.noreply.github.com>
2021-02-21 01:05:50 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Hello!")
+})
 basic.showLeds(`
     . . # . .
     . . # . .
